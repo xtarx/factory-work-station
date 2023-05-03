869e31b551e7fe6435b59615a4e85f099380c3fd
t-indie <t.shaban.kanzi@gmail.com>
2023-05-03 19:48:26 +0200

    chore: initial skeleton

---
+++ src/main/java/com/factory/workstation/WorkstationApplication.java
@@ -0,0 +1,13 @@
+package com.factory.workstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/workstation/WorkstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.workstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
